0c788e98a368f5d671c0eaf0e40b483134e7bc8c
pamuiafrika <pamuiafrika@gmail.conm>
2025-06-27 09:38:23 +0300

    new updates

--- config/settings.py
+++ config/settings.py
@@ -114,8 +114,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # for development
 ]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"  # for collectstatic in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
